abbb03f63df914ceab0581d1a17dfd81b0625b4c
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-04 11:00:15 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -394,12 +394,7 @@ if result:
             name="K線",
             increasing_line_color='#FF3131', # 漲：紅
             decreasing_line_color='#00FF00',  # 跌：綠
-            hovertemplate=(
-                "開盤: %{open:.1f}" +
-                "最高: %{high:.1f}" +
-                "最低: %{low:.1f}" +
-                "收盤: %{close:.1f}"
-            )
+            hovertemplate='%{open:.1f} <br>%{high:.1f} <br>%{low:.1f} <br>%{close:.1f}'
         ))
         # 疊加 MA 線段 (5, 10, 20, 60, 120)
         # 注意：請確保 get_stock_data 函式內有計算這些 MA 欄位
