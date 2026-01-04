2568968cef2709d72be45f47c62a09b5cb58d3e1
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-04 11:34:20 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -393,8 +393,8 @@ if result:
             low=df['Low'], close=df['Close'],
             name="K線",
             increasing_line_color='#FF3131', # 漲：紅
-            decreasing_line_color='#00FF00',  # 跌：綠
-            hovertemplate='%{open:.1f} <br><b>%{high:.1f}</b><br>%{low:.1f}</b><br>%{close:.1f}'
+            decreasing_line_color='#00FF00'  # 跌：綠
+            #hovertemplate='%{open:.1f} <br><b>%{high:.1f}</b><br>%{low:.1f}</b><br>%{close:.1f}'
         ))
         # 疊加 MA 線段 (5, 10, 20, 60, 120)
         # 注意：請確保 get_stock_data 函式內有計算這些 MA 欄位
