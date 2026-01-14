0283a1d8c627e5588c196fe75a33b9978e2a60b2
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-14 23:25:52 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -1197,6 +1197,13 @@ if result:
             spikecolor="white", # 設定為白色
             spikedash="solid"   # 實線 (若要虛線改為 dash)
         )
+        yaxis=dict(
+            showspikes=True, # 顯示指引線
+            spikemode="across", # 穿過整個圖表
+            spikethickness=1,
+            spikecolor="white", # 設定為白色
+            spikedash="solid"   # 實線 (若要虛線改為 dash)
+        )
     )    
         # 如果有開啟副圖，額外設定副圖的 Y 軸指引線顏色為白色
     if show_sub_chart:
