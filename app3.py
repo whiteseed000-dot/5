101a1d08dcfa4a4b311b9e4de0a790c63352efed
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-17 17:33:32 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -866,23 +866,7 @@ def get_stock_data(ticker, years, time_frame="日", use_adjusted_price=False): #
         for p in ma_periods:
             df[f'MA{p}'] = df['Close'].rolling(window=p).mean() 
             df[f'MA{p}_slope'] = df[f'MA{p}'].diff()
-             # --買/賣箭頭       
-            df['buy_signal'] = (
-                (df['Close'] > df['MA60']) &
-                (df['MA60_slope'] > 0) &
-                (df['Close'] > df['MA5']) &
-                (df['MA5_slope'] > 0) &
-                (df['Close'] > df['Open']) &              # 本K紅
-                (df['Close'].shift(1) < df['Open'].shift(1))  # 前K黑
-            )
-            df['sell_signal'] = (
-                (df['Close'] < df['MA60']) &
-                (df['MA60_slope'] < 0) &
-                (df['Close'] < df['MA5']) &
-                (df['MA5_slope'] < 0) &
-                (df['Close'] < df['Open']) &               # 本K黑
-                (df['Close'].shift(1) > df['Open'].shift(1))   # 前K紅
-            )
+
         
         df.attrs['ma_periods'] = ma_periods
 
@@ -1138,7 +1122,30 @@ if result:
             decreasing_line_color='#00FF00'  # 跌：綠
             # 自定義 K 線懸浮文字格式
         ))
-     
+        
+        fig.add_trace(go.Scatter(
+            x=df.loc[df['buy_signal'], 'Date'],
+            y=df.loc[df['buy_signal'], 'Low'] * 0.995,   # 當日最低價下方
+            mode='markers',
+            marker=dict(
+                symbol='triangle-up',
+                size=12,
+                color='#00FF00'
+            ),
+            name='買入訊號'
+        ))
+        fig.add_trace(go.Scatter(
+            x=df.loc[df['sell_signal'], 'Date'],
+            y=df.loc[df['sell_signal'], 'High'] * 1.005, # 當日最高價上方
+            mode='markers',
+            marker=dict(
+                symbol='triangle-down',
+                size=12,
+                color='#FF3131'
+            ),
+            name='賣出訊號'
+        ))
+
 
         # 2. 疊加 MA 線段 (5, 10, 20, 60, 120)
         # 從 df 取回 MA 週期（不會 NameError）
