a427d748898d43d50219c5238424cd7481afd3ff
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-17 22:51:43 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -946,7 +946,6 @@ def get_stock_data(ticker, years, time_frame="日", use_adjusted_price=False): #
             # ===== 新增：多指標確認 =====
         
             # ⑤ MACD 動能轉空
-            (curr['MACD'] > prev['MACD'])
            
             # ⑥ RSI 在空方區、非超賣
            # (df['RSI14'] < 50) &
