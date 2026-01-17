a7d48cf71bd04c48cdc8f573d7efa039737c4ec6
whiteseed000-dot <whiteseed000@gmail.com>
2026-01-17 16:45:51 +0800

    Update app3.py

--- app3.py
+++ app3.py
@@ -1157,30 +1157,29 @@ if result:
                 fig.add_trace(go.Scatter(x=df['Date'], y=df[col], name=name, line=dict(color=color, width=1.2), hovertemplate='%{y:.1f}'
                           
         ))
-
         fig.add_trace(go.Scatter(
-        x=df.index[df['buy_signal']],
-        y=df['Low'][df['buy_signal']] * 0.995,
-        mode='markers',
-        marker=dict(
-            symbol='triangle-up',
-            size=12,
-            color='green'
-        ),
-        name='買入'
+            x=df.loc[df['buy_signal'], 'Date'],
+            y=df.loc[df['buy_signal'], 'Low'] * 0.995,   # 當日最低價下方
+            mode='markers',
+            marker=dict(
+                symbol='triangle-up',
+                size=12,
+                color='#00FF00'
+            ),
+            name='買入訊號'
+        ))
+        fig.add_trace(go.Scatter(
+            x=df.loc[df['sell_signal'], 'Date'],
+            y=df.loc[df['sell_signal'], 'High'] * 1.005, # 當日最高價上方
+            mode='markers',
+            marker=dict(
+                symbol='triangle-down',
+                size=12,
+                color='#FF3131'
+            ),
+            name='賣出訊號'
         ))
 
-        fig.add_trace(go.Scatter(
-        x=df.index[df['sell_signal']],
-        y=df['High'][df['sell_signal']] * 1.005,
-        mode='markers',
-        marker=dict(
-            symbol='triangle-down',
-            size=12,
-            color='red'
-        ),
-        name='賣出'
-        ))
 
         fig.update_layout(xaxis_rangeslider_visible=False) # 隱藏下方的滑桿
 
